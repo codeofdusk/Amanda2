200aac4ef3b6d9f7504ed4fb0839acf1263261c9
Bill Dengler <codeofdusk@gmail.com>
2016-09-19 01:36:14 +0000

    Fix syntax errors.

--- amanda2.py
+++ amanda2.py
@@ -7,7 +7,7 @@ class MySkype(skpy.SkypeEventLoop):
             #Get the message
             q=event.msg.content
             #Is this a Wolfram command?
-            if hasattr(settings,wolframkey) and str(q).startswith("?"):
+            if hasattr(settings,"wolframkey") and str(q).startswith("?"):
                 event.msg.chat.setTyping(active=True)
                 try:
                     return event.msg.chat.sendMsg(wolfram.query(q[1:]))
@@ -21,12 +21,12 @@ else:
     s=MySkype(settings.skypeuser,settings.skypepass,autoAck=True)
     #build the startup string.
     #Do we have a message of the day?
-    if hasattr(settings,motd):
+    if hasattr(settings,"motd"):
         startstr=settings.motd
     else:
         startstr="I Am Completely Operational, And All My Circuits Are Functioning Perfectly!"
     #Do we have Wolfram?
-    if hasattr(settings,wolframkey):
+    if hasattr(settings,"wolframkey"):
         startstr+=" Send ? followed by a query to send to Wolfram Alpha."
     #Send it out.
 s.chats[settings.window].sendMsg(startstr)
