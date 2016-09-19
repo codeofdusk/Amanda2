2eacc2c9e9e44b5914cdee59c1740b77594e4346
Bill Dengler <codeofdusk@gmail.com>
2016-09-19 03:01:06 +0000

    Add lifespan special command.

--- amanda2.py
+++ amanda2.py
@@ -15,7 +15,11 @@ class MySkype(skpy.SkypeEventLoop):
                     import traceback
                     return event.msg.chat.sendMsg(traceback.format_exc())
             #Is this a special command?
-
+            if q.startswith("!"):
+                #Drop the exclamation point.
+                q=q[1:]
+                if q == "lifespan":
+                    return event.msg.chat.sendMsg("My time of death has been calculated as " + str(s.conn.tokenExpiry['skype']) + ". At that time, my administrator will need to bring me back to life!")
 if settings.microsoft:
     raise NotImplementedError
 else:
@@ -29,6 +33,8 @@ else:
     #Do we have Wolfram?
     if hasattr(settings,"wolframkey"):
         startstr+=" Send ? followed by a query to send to Wolfram Alpha."
+    #Add message about special commands.
+    startstr+=" Prefix special commands with ! (documentation coming soon!)"
     #Send it out.
 s.chats[settings.window].sendMsg(startstr)
 s.loop()
