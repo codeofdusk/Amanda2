6cb0993b729fe9b2d4349cf4670965c90c1f0198
Bill Dengler <codeofdusk@gmail.com>
2016-09-19 01:17:59 +0000

    Reorganize settings, disable Wolfram support by default.

--- settings_default.py
+++ settings_default.py
@@ -1,12 +1,15 @@
 #Amanda settings file
-#Before using Amanda, fill in these settings.
+#Before using Amanda, fill in these settings, then copy this file to settings.py.
+#Skype settings
 #Does Amanda log into Skype using a Microsoft account?
 microsoft=False
 #Amanda's Skype name/Microsoft account email
 skypeuser=""
 #Amanda's Skype/Microsoft password
 skypepass=""
-#Amanda's Wolfram Alpha API key
-wolframkey=""
 #Skype cloud identifier of the window where Amanda resides.
 window=""
+
+#Wolfram Alpha
+#To enable Wolfram Alpha support, uncomment the following line and specify Amanda's Wolfram Alpha app ID.
+#wolframkey=""
