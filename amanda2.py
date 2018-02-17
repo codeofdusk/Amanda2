1c048960098ab061df90d831cb15a51bba4508b3
Bill Dengler <codeofdusk@gmail.com>
2018-02-17 10:01:28 +0000

    Beginnings of new abstraction of design (separation of functionality into plugins). All features have been removed and will be re-introduced in subsequent commits. Topic expery and the lifespan command will not be re-implemented.

--- amanda2.py
+++ amanda2.py
@@ -9,61 +9,45 @@ class MySkype(skpy.SkypeEventLoop):
         if isinstance(event,skpy.SkypeNewMessageEvent) and event.msg.chat.id == settings.window:
             #Get the message
             q=str(event.msg.content)
-            #Is this a Wolfram command?
-            if hasattr(settings,"wolframkey") and q.startswith("?"):
-                event.msg.chat.setTyping(active=True)
-                try:
-                    return event.msg.chat.sendMsg(wolfram.query(q[1:]))
-                except:
-                    import traceback
-                    return event.msg.chat.sendMsg(traceback.format_exc())
-            #Is this a special command?
-            if q.startswith("!"):
-                #Drop the exclamation point.
-                q=q[1:]
-                if q == "lifespan":
-                    return event.msg.chat.sendMsg("My time of death has been calculated as " + str(s.conn.tokenExpiry['skype']) + ". At that time, my administrator will need to bring me back to life!")
-                if q.startswith("expire"):
-                    #Split the date string.
-                    qt=q.split(" ")
-                    if len(qt) < 2:
-                        return event.msg.chat.sendMsg("Please specify a time interval!")
-                    datestr=' '.join(qt[1:])
-                    dateobj=dateparser.parse(datestr)
-                    if dateobj == None:
-                        return event.msg.chat.sendMsg("Invalid expiration date.")
-                    event.msg.chat.sendMsg("Topic set to expire on " + str(dateobj) + ".")
-                    self.temp_topic=event.msg.chat.topic
-                    self.topic_expiry=dateobj
-                if q.startswith("geo"):
-                    if len(q.split(" ")) != 2: return event.msg.chat.sendMsg("Usage: !geo <hostname or IP>")
-                    else: return event.msg.chat.sendMsg(geo.as_string(geo.lookup(q.split(" ")[1])))
-    def cycle(self):
-        skpy.SkypeEventLoop.cycle(self)
-        #Known bug: topic expiry doesn't work unless system time is in UTC
-        if hasattr(self,'temp_topic') and self.temp_topic != None and hasattr(self,'topic_expiry') and self.topic_expiry != None and self.chats[settings.window].topic == self.temp_topic and datetime.now() > self.topic_expiry:
-            if hasattr(settings,'default_topic'):
-                self.chats[settings.window].setTopic(settings.default_topic)
-            else:
-                self.chats[settings.window].setTopic("Hangin' in Suspense")
-            self.temp_topic=None
-            self.topic_expiry=None
+            try:
+                # First attempt to call plugin explicitly
+                if hasattr(settings,'allow_explicit') and settings.allow_explicit and q.startswith("!"):
+                    # Get the plugin name and args
+                    qt=q[1:].split(" ")
+                    pn=qt[0]
+                    pa=' '.join(qt[1:])
+                    # Search for the plugin
+                    for plugin in settings.plugins:
+                        if hasattr(plugin,'name') and plugin.name == pn:
+                            event.msg.chat.setTyping(active=True)
+                            return event.msg.chat.sendMsg(plugin.run(pa,explicit=True))
+                    return event.msg.chat.sendMsg("Invalid command.")
+                # Attempt to call plugin implicitly
+                if hasattr(settings,'allow_implicit') and settings.allow_implicit:
+                    for plugin in settings.plugins:
+                        if hasattr(plugin,'match'):
+                            m = plugin.match(q)
+                            if m:
+                                event.msg.chat.setTyping(active=True)
+                                return event.msg.chat.sendMsg(plugin.run(m))
+            except:
+                import traceback
+                return event.msg.chat.sendMsg(traceback.format_exc())
 
 if settings.microsoft:
     raise NotImplementedError
+s=MySkype(settings.skypeuser,settings.skypepass,autoAck=True)
+#build the startup string.
+#Do we have a message of the day?
+if hasattr(settings,"motd"):
+    startstr=settings.motd
 else:
-    s=MySkype(settings.skypeuser,settings.skypepass,autoAck=True)
-    #build the startup string.
-    #Do we have a message of the day?
-    if hasattr(settings,"motd"):
-        startstr=settings.motd
-    else:
-        startstr="I Am Completely Operational, And All My Circuits Are Functioning Perfectly!"
-    #Do we have Wolfram?
-    if hasattr(settings,"wolframkey"):
-        startstr+=" Send ? followed by a query to send to Wolfram Alpha."
-    #Add message about special commands.
-    startstr+=" Prefix special commands with ! (documentation coming soon!)"
-    #Send it out.
-s.chats[settings.window].sendMsg(startstr)
+    startstr="I Am Completely Operational, And All My Circuits Are Functioning Perfectly!"
+# Advertise enabled plugins
+if hasattr(settings,'advertise_plugins') and settings.advertise_plugins:
+    for plugin in settings.plugins:
+        if hasattr(plugin,'ad'):
+            startstr+=" " + plugin.ad
+#Send it out.
+s.chats[settings.window].sendMsg(startstr.strip())
 s.loop()
--- settings_default.py
+++ settings_default.py
@@ -3,10 +3,13 @@
 #General settings
 #Message Of The Day
 #If Amanda should send a custom message on startup, uncomment the following line and specify your message. Leave commented to send the default message.
-    #motd=""
+#motd=""
 #Custom Huh Messages
-    #Uncomment and supply a random list of messages to send when Amandadoesn't understand a message.
+#Uncomment and supply a random list of messages to send when Amanda doesn't understand a message.
 #huh_messages=[]
+# Plugin advertisement
+# Comment out (or set to false) the following line to disable advertisement of enabled plugins (using text supplied with each plugin) when Amanda starts. If you have enabled a large number of plugins, disabling this advertisement may be a good idea to avoid sending a large message on startup.
+advertise_plugins=True
 #Skype settings
 #Does Amanda log into Skype using a Microsoft account?
 microsoft=False
@@ -16,8 +19,17 @@ skypeuser=""
 skypepass=""
 #Skype cloud identifier of the window where Amanda resides.
 window=""
-#When using Skype topic expiry, the topic Amanda should set when the current one expires (leave commented to use the default).
-#defaulttopic = ""
-#Wolfram Alpha
-#To enable Wolfram Alpha support, uncomment the following line and specify Amanda's Wolfram Alpha app ID.
-#wolframkey=""
+
+# Plugins
+# Plugins add functionality to Amanda. They can be enabled and configured here.
+plugins=[
+
+]
+
+# Advanced options
+# Explicit plugin use
+# Comment out (or set to False) the following line to disable the ability to call plugins explicitly (entering !pluginname args to use an enabled plugin). Disabling this feature may be useful in special circumstances but it should generally be left enabled.
+allow_explicit=True
+# Implicit plugin use
+# Comment out (or set to False) the following line to disable the ability to call plugins implicitly (using custom match patterns). Disabling this feature may be useful in special circumstances but it should generally be left enabled.
+allow_implicit=True
