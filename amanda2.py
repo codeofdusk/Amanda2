2456d517ea0dcfc0a6f63d42b071a6c147045c26
Bill Dengler <codeofdusk@gmail.com>
2018-03-15 03:32:22 +0000

    Initial cleanup of style of AManda core.

--- amanda2.py
+++ amanda2.py
@@ -1,31 +1,33 @@
-from request import request
+"Amanda's main module and the entry point for the program."
 from threading import Thread
 import settings
 
+
 def build_startup_message():
-    #build the startup string.
-    #Do we have a message of the day?
-    if hasattr(settings,"motd"):
-        startstr=settings.motd
+    """Build the startup string, an announcement sent to newly-connected
+users or through newly-initialized drivers."""
+    # Do we have a message of the day?
+    if hasattr(settings, "motd"):
+        startstr = settings.motd
     else:
-        startstr="I Am Completely Operational, And All My Circuits Are Functioning Perfectly!"
+        startstr = "I Am Completely Operational, And All My Circuits Are Functioning Perfectly!"
     # Advertise enabled plugins
-    if hasattr(settings,'advertise_plugins'):
+    if hasattr(settings, 'advertise_plugins'):
         for plugin in settings.plugins:
-            if plugin.name in settings.advertise_plugins or len(settings.advertise_plugins) < 1:
-                if hasattr(plugin,'ad'):
-                    startstr+=" " + plugin.ad
+            if plugin.name in settings.advertise_plugins or len(
+                    settings.advertise_plugins) < 1:
+                if hasattr(plugin, 'ad'):
+                    startstr += " " + plugin.ad
     return startstr
+
 
 if __name__ == '__main__':
     # Start all drivers
-    if not hasattr(settings,'drivers') or len(settings.drivers) < 1:
+    if not hasattr(settings, 'drivers') or len(settings.drivers) < 1:
         raise ValueError("You must configure at least one driver.")
     # Build the startup string
-    ss=build_startup_message()
+    SS = build_startup_message()
     for driver in settings.drivers:
         Thread(target=driver.run).start()
-        if hasattr(driver,'announce'):
-            driver.announce(ss)
-        else:
-            driver.say(ss,request=None)
+        if hasattr(driver, 'announce'):
+            driver.announce(SS)
--- request.py
+++ request.py
@@ -1,55 +1,65 @@
-import settings
+"Contains the Request class."
 import random
+import settings
+
+
 class request(object):
-    def __init__(self,content,driver=None,*args,**kwargs):
+    "Contains attributes for handling user requests and returning responses. Instantiate this class to make a request."
+    def __init__(self, content, driver=None, *args, **kwargs):
         "Take a string and return a response (if available) or False for no response."
-        self.content=content
-        self.driver=driver
-        self.accepted=False
-        self.response=None
+        self.content = content
+        self.driver = driver
+        self.accepted = False
+        self.response = None
         # Expose extra args
-        self.args=args
-        self.kwargs=kwargs
+        self.args = args
+        self.kwargs = kwargs
         try:
             # First attempt to call plugin explicitly
-            if hasattr(settings,'allow_explicit') and settings.allow_explicit and self.content.startswith("!"):
+            if hasattr(
+                    settings,
+                    'allow_explicit') and settings.allow_explicit and self.content.startswith("!"):
                 self.accept()
                 # Get the plugin name and args
-                qt=self.content[1:].split(" ")
-                pn=qt[0]
-                pa=' '.join(qt[1:])
+                qt = self.content[1:].split(" ")
+                pn = qt[0]
+                pa = ' '.join(qt[1:])
                 # Search for the plugin
                 for plugin in settings.plugins:
-                    if hasattr(plugin,'name') and plugin.name.lower() == pn.lower():
-                        self.response = plugin.run(pa,explicit=True)
+                    if hasattr(plugin,
+                               'name') and plugin.name.lower() == pn.lower():
+                        self.response = plugin.run(pa, explicit=True)
             # Attempt to call plugin implicitly
-            if hasattr(settings,'allow_implicit') and settings.allow_implicit:
+            if hasattr(settings, 'allow_implicit') and settings.allow_implicit:
                 for plugin in settings.plugins:
-                    if hasattr(plugin,'match'):
+                    if hasattr(plugin, 'match'):
                         m = plugin.match(self.content)
                         if m:
                             self.accept()
                             self.response = plugin.run(m)
-        except:
+        except BaseException:
             import traceback
             self.response = traceback.format_exc()
         finally:
             self.finalize()
+
     def __str__(self):
         if not self.response:
             # do we have huh messages?
-            if hasattr(settings,'huh_messages'):
+            if hasattr(settings, 'huh_messages'):
                 self.response = random.choice(settings.huh_messages)
             else:
                 self.response = "I don't understand."
         return self.response
+
     def accept(self):
-        if hasattr(self.driver,'working'):
-            self.driver.working(True,request=self)
-        self.accepted=True
+        if hasattr(self.driver, 'working'):
+            self.driver.working(True, request=self)
+        self.accepted = True
+
     def finalize(self):
         if self.accepted:
-            if hasattr(self.driver,'say'):
-                self.driver.say(str(self),request=self)
-            if hasattr(self.driver,'working'):
-                self.driver.working(False,request=self)
+            if hasattr(self.driver, 'say'):
+                self.driver.say(str(self), request=self)
+            if hasattr(self.driver, 'working'):
+                self.driver.working(False, request=self)
