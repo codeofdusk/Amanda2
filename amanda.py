c3cbedd3f82cd111485a5b43514bbf337e8d5001
Bill Dengler <codeofdusk@gmail.com>
2020-06-11 06:05:07 -0400

    Daemonize and join driver threads.

--- amanda.py
+++ amanda.py
@@ -5,24 +5,38 @@ import components
 import config
 import utils
 
-if __name__ == '__main__':
-    print("Amanda: a simple, extensible chatbot framework.\nCopyright 2018 - Bill Dengler <codeofdusk@gmail.com>. Licensed under MIT.")
+if __name__ == "__main__":
+    print(
+        "Amanda: a simple, extensible chatbot framework.\nCopyright 2018 - Bill Dengler <codeofdusk@gmail.com>. Licensed under MIT."
+    )
+
     # Prepare config
     config.load()
+
     # Instantiate components
     if components.load():
         print(
             "New plugins and/or drivers have been discovered! Edit the configuration file to enable or customize them."
         )
+
     # Start all drivers
-    if not hasattr(components, 'drivers') or len(components.drivers) < 1:
+    threads = []
+    if not hasattr(components, "drivers") or len(components.drivers) < 1:
         raise ValueError("You must configure at least one driver.")
     for driver in components.drivers:
-        Thread(target=driver.run).start()
-        short = config.conf['general']['sendmotd'] == 'retracted' or getattr(driver,'short_announcements',False)
-        ss=utils.build_startup_message(short=short)
+        tr = Thread(target=driver.run)
+        threads.append(tr)
+        tr.daemon = True
+        tr.start()
+        short = config.conf["general"]["sendmotd"] == "retracted" or getattr(
+            driver, "short_announcements", False
+        )
+        ss = utils.build_startup_message(short=short)
         if ss is not None:
             try:
                 driver.announce(ss)
             except (AttributeError, NotImplementedError):
                 continue
+
+    for tr in threads:
+        tr.join()
